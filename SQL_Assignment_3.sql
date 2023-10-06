f3a18e5d0206bf7825da4d591bcc1e28934b0707
hayl-y <73183717+hayl-y@users.noreply.github.com>
2023-10-06 21:18:17 +0100

    Updated Code
    Added functionality

--- SQL_Assignment_3.sql
+++ SQL_Assignment_3.sql
@@ -34,10 +34,11 @@ CREATE TABLE Authors(
 
 CREATE TABLE Books( -- Links to Authors Table
     BookID INT PRIMARY KEY,
-    Title VARCHAR(255) NOT NULL,
+    Title VARCHAR(250) NOT NULL,
     ISBN VARCHAR(14) UNIQUE, -- International Standard Book Number [13 numbers and one dash hence varchar]
     PublicationYear INT,
     AuthorID INT,
+    Availability INT,
     FOREIGN KEY (AuthorID) REFERENCES Authors(AuthorID)
 );
 
@@ -52,6 +53,7 @@ CREATE TABLE Loans( -- Links with Books & Users Tables
     BookID INT CHECK (BookID > 100),
     UserID INT,
     LoanDate DATE,
+    DueDate DATE,
     ReturnDate DATE,
     FOREIGN KEY (BookID) REFERENCES Books(BookID),
     FOREIGN KEY (UserID) REFERENCES Users(UserID)
@@ -75,20 +77,20 @@ VALUES
     (10, 'Milly', 'Bobby Brown');
 
 INSERT INTO Books 
-	(BookID, Title, ISBN, PublicationYear, AuthorID)
+	(BookID, Title, ISBN, PublicationYear, AuthorID, Availability)
 VALUES
-    (101, 'The White Princess', '978-0857207517', 2013, 1),
-    (102, 'Winnie The Pooh', '978-1405281317', 1926, 2),
-    (103, 'To Kill a Mockingbird', '978-0099549482', 1960, 3),
-    (104, 'Mythos', '978-0718188726', 2017, 4),
-    (105, 'Glucose Revolution', '978-1780725239', 2022, 5),
-    (106, 'Noughts & Crosses', '978-0141378640', 2017, 6),
-    (107, 'The Great Gatsby', '978-1840221886', 1925, 7),
-    (108, 'Pride and Prejudice', '978-1840221930', 1913, 8),
-    (109, 'The Last Devil to Die', '978-0241512449', 2023, 9),
-    (110, 'Nineteen Steps', '978-0008530266', 2023, 10),
-    (111, 'Troy', '978-1405944465', 2020, 4),
-    (112, 'Heroes', '978-1405940368', 2018, 4);
+    (101, 'The White Princess', '978-0857207517', 2013, 1, 1),
+    (102, 'Winnie The Pooh', '978-1405281317', 1926, 2, 1),
+    (103, 'To Kill a Mockingbird', '978-0099549482', 1960, 3, 1),
+    (104, 'Mythos', '978-0718188726', 2017, 4, 1),
+    (105, 'Glucose Revolution', '978-1780725239', 2022, 5, 1),
+    (106, 'Noughts & Crosses', '978-0141378640', 2017, 6, 1),
+    (107, 'The Great Gatsby', '978-1840221886', 1925, 7, 1),
+    (108, 'Pride and Prejudice', '978-1840221930', 1913, 8, 1),
+    (109, 'The Last Devil to Die', '978-0241512449', 2023, 9, 1),
+    (110, 'Nineteen Steps', '978-0008530266', 2023, 10, 1),
+    (111, 'Troy', '978-1405944465', 2020, 4, 1),
+    (112, 'Heroes', '978-1405940368', 2018, 4, 1);
     
 
 INSERT INTO Users
@@ -97,26 +99,26 @@ VALUES
 	(1001, 'Pink', 'Smith'), 
 	(1002, 'Ruby Frost', 'Gala'), 
 	(1003, 'Oakley', 'Cox'),
-    (1004, 'Snap', 'Cortland'),
-    (1005, 'Mutsu', 'Gravenstein'),
-    (1006, 'Macoun', 'Spitzenburg'),
-    (1007, 'Gran', 'Mcintosh'),
-    (1008, 'Spy', 'York');
+	(1004, 'Snap', 'Cortland'),
+	(1005, 'Mutsu', 'Gravenstein'),
+	(1006, 'Macoun', 'Spitzenburg'),
+	(1007, 'Gran', 'Mcintosh'),
+	(1008, 'Spy', 'York');
     
 INSERT INTO Loans -- Date format: YYYY-MM-DD
-    (LoanID, BookID, UserID, LoanDate, ReturnDate)
+    (LoanID, BookID, UserID, LoanDate, DueDate, ReturnDate)
 VALUES
-    (201, 101, 1001, '2023-08-01', '2023-08-15'),
-    (202, 104, 1005, '2023-08-17', '2023-08-31'),
-    (203, 102, 1006, '2023-07-15', '2023-07-29'),
-    (204, 107, 1003, '2023-09-25', '2023-10-09'),
-    (205, 106, 1005, '2023-09-25', '2023-10-09'),
-    (206, 103, 1008, '2023-09-30', '2023-10-14'),
-    (207, 104, 1006, '2023-09-30', '2023-10-14'),
-    (208, 104, 1004, '2023-09-30', '2023-10-14'),
-    (209, 102, 1003, '2023-09-25', '2023-10-09'),
-    (210, 111, 1006, '2023-09-30', '2023-10-14'),
-    (211, 112, 1001, '2023-09-30', '2023-10-14');
+    (201, 101, 1001, '2023-08-01', '2023-08-15', '2023-08-15'),
+    (202, 104, 1005, '2023-08-17', '2023-08-31', '2023-08-31'),
+    (203, 102, 1006, '2023-07-15', '2023-07-29', '2023-07-29'),
+    (204, 107, 1003, '2023-09-25', '2023-10-09', '2023-10-06'),
+    (205, 106, 1005, '2023-09-25', '2023-10-09', '2023-10-09'),
+    (206, 103, 1008, '2023-09-30', '2023-10-14', NULL),
+    (207, 104, 1006, '2023-09-30', '2023-10-14', NULL),
+    (208, 104, 1004, '2023-09-30', '2023-10-14', NULL),
+    (209, 102, 1003, '2023-09-25', '2023-10-09', NULL),
+    (210, 111, 1006, '2023-09-30', '2023-10-14', NULL),
+    (211, 112, 1001, '2023-09-30', '2023-10-14', NULL);
 
 
 
@@ -199,13 +201,34 @@ SELECT u.FirstName, u.Surname
 From Users u
 ORDER BY u.Surname asc;
 
-SELECT b.Title AS BookTitle, b.GenreID -- NB: Troy & Heroes do NOT have a genre attached (yet) hence will be NULL
-From Books b
-ORDER BY b.Title asc;
+-- SELECT b.Title AS BookTitle, b.GenreID -- NB: Troy & Heroes do NOT have a genre attached (yet) hence will be NULL
+-- From Books b
+-- ORDER BY b.Title asc;
 
-SELECT b.Title AS BookTitle, g.GenreName, b.GenreID
-FROM Books b
-RIGHT JOIN Genres g ON b.GenreID = g.GenreID; -- To show all books catergorised into their genres, and those that need to be sorted
+-- SELECT b.Title AS BookTitle, g.GenreName, b.GenreID
+-- FROM Books b
+-- RIGHT JOIN Genres g ON b.GenreID = g.GenreID; -- To show all books catergorised into their genres, and those that need to be sorted
+
+SELECT *,
+    CASE
+        WHEN ReturnDate IS NOT NULL THEN DATEDIFF(ReturnDate, LoanDate)
+        ELSE DATEDIFF(CURDATE(), LoanDate)
+    END AS 'Days On Loan',
+    CURDATE() AS 'Current Date',
+    CASE
+        WHEN ReturnDate IS NULL THEN 0
+        ELSE 1
+    END AS 'Availability'
+FROM Loans l
+ORDER BY l.LoanDate ASC;
+
+SELECT u.FirstName AS User_FirstName, 
+u.Surname AS User_Surname, 
+COUNT(CASE WHEN l.ReturnDate IS NULL THEN 1 ELSE NULL END) AS NumberOfLoans
+FROM Users u
+LEFT JOIN Loans l ON u.UserID = l.UserID
+GROUP BY u.FirstName, u.Surname
+ORDER BY u.Surname ASC;
 
 SELECT b.Title, b.PublicationYear
 FROM Books b
@@ -216,13 +239,6 @@ SELECT b.Title AS BookTitle, a.FirstName AS Author_FirstName, a.Surname AS Autho
 FROM Books b
 RIGHT JOIN Authors a ON b.AuthorID = a.AuthorID -- Want to show all books, will help highlight which book author's need to be added
 ORDER BY a.Surname asc;
-
-SELECT u.FirstName AS User_FirstName, u.Surname AS User_Surname, 
-COUNT(l.LoanID) AS NumberOfLoans
-FROM Users u
-LEFT JOIN Loans l ON u.UserID = l.UserID --  All Loans to be mentioned, help library track how many books are out
-GROUP BY u.FirstName, u.Surname
-ORDER BY u.Surname asc;
 
 SELECT ROUND(AVG(b.PublicationYear)) AS AveragePublicationYear -- Round to an int. as it is a year
 FROM Books b;
@@ -245,9 +261,9 @@ DELETE FROM Loans
 WHERE ReturnDate <= DATE_SUB(NOW(), INTERVAL 30 DAY);
 
 -- Check data deleted [WILDCARD REQUIRED] --
-SELECT *
-From Loans l
-ORDER BY l.LoanDate asc;
+-- SELECT *
+-- From Loans l
+-- ORDER BY l.LoanDate asc;
 
 
 
@@ -259,8 +275,8 @@ ALTER TABLE Books
 DROP COLUMN GenreID;
 
 -- Check the column has been dropped [WILDCARD REQUIRED] --
-SELECT *
-FROM Books; 
+-- SELECT *
+-- FROM Books; 
 
 CREATE TABLE BooksGenres (
     BookID INT,
@@ -287,8 +303,8 @@ VALUES
     (112, 304);
 
 -- Check Whole Book Genres Table [WILDCARD REQUIRED] --
-SELECT *
-FROM BooksGenres bg;
+-- SELECT *
+-- FROM BooksGenres bg;
 
 SELECT bg.BookID, b.Title AS BookTitle, g.GenreName, bg.GenreID
 FROM BooksGenres bg
@@ -299,11 +315,38 @@ ORDER BY g.GenreName asc;
 
 
 -- Store Procedure/ Function
+-- Get latest loan number, increase by one, insert a new loan into table, whilst making sure the book is available
 DELIMITER //
-CREATE PROCEDURE 
-CheckAvailability(
-IN bookTitle VARCHAR(255), 
-OUT available BOOLEAN)
+
+CREATE PROCEDURE UpdateBookAvailability(
+    IN bookID INT,
+    IN userID INT,
+    IN loanDate DATE,
+    IN dueDate DATE,
+    IN returnDate DATE)
+BEGIN
+    DECLARE newLoanID INT;
+
+    SELECT IFNULL(MAX(LoanID), 0) + 1 INTO newLoanID FROM Loans;
+
+    INSERT INTO Loans (LoanID, BookID, UserID, LoanDate, DueDate, ReturnDate)
+    VALUES (newLoanID, bookID, userID, loanDate, dueDate, returnDate);
+
+    UPDATE Books
+    SET Availability = 0
+    WHERE BookID = bookID;
+    
+    SELECT newLoanID AS NewLoanNumber;
+END //
+
+DELIMITER ;
+
+CALL UpdateBookAvailability(101, 1005, '2023-10-01', '2023-10-15', '2023-10-15');
+ 
+DELIMITER //
+CREATE PROCEDURE CheckAvailability(
+	IN bookTitle VARCHAR(250), 
+	OUT available BOOLEAN)
 BEGIN
     DECLARE bookCount INT;
     SELECT COUNT(*) INTO bookCount FROM Loans l
@@ -318,10 +361,35 @@ BEGIN
 END //
 DELIMITER ;
 
-SET @isAvailable = FALSE;
-CALL CheckAvailability('The Great Gatsby', @isAvailable);
-SELECT @isAvailable AS 'Is The Great Gatsby Available?';
+CALL CheckAvailability(101, available)
 
+DELIMITER //
+CREATE PROCEDURE AddNewLoan(
+	IN bookID INT,
+	IN userID INT,
+	IN loanDate DATE,
+	IN dueDate DATE)
+BEGIN
+    DECLARE newLoanID INT;
+    
+    SELECT IFNULL(MAX(LoanID), 0) + 1 INTO newLoanID FROM Loans;
+    
+    INSERT INTO Loans (LoanID, BookID, UserID, LoanDate, DueDate)
+    VALUES (newLoanID, bookID, userID, loanDate, dueDate);
+    
+    UPDATE Books
+    SET Availability = 0
+    WHERE BookID = bookID;
+    
+    SELECT newLoanID AS NewLoanNumber;
+END //
+DELIMITER ;
 
--- Get latest loan number, increase by one, insert a new loan into table, check if book is available, insert loan book into table
-START TRANSACTION;
+CALL AddNewLoan(101, 1003, '2023-10-01', '2023-10-15');
+
+-- Checks new loan has been added to the table [WILDCARD REQUIRED] --
+SELECT *
+FROM Loans;
+
+SELECT * 
+FROM Books;
